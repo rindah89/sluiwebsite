4da17608be40d283639f6dc872db5e02ea03c6ac
NjohPrince <jufredprince@gmail.com>
2022-12-08 04:47:24 +0100

    router setup and updated readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./pages/about/About";
+import NotFound from "./pages/error/404";
+
+// pages
+import Landing from "./pages/landing/Landing";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/about-st-louis" element={<About />} />
+
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
